8648b9fe6834a32d90cd5847ff9e9b9f7167bf4f
Raphael Augusto <augustorapha27@gmail.com>
2026-02-12 22:58:15 -0300

    fix(header): modificado o router da logo com a condição de caso o usuário esteja logado ou não.

--- frontend/src/components/layout/header/header.ts
+++ frontend/src/components/layout/header/header.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../auth/service/auth-service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, RouterModule ],
   template: `
     <header class="header">
       <div class="main-header">
-          <a href="/" class="logo-wrapper">
+          <a [routerLink]="authService.isAuthenticated() ? '/home' : '/'" class="logo-wrapper">
               <img src="ticket.png" class="logo-img">
               <h1 class="logo-title">Cinema-Tickets</h1>
           </a>
